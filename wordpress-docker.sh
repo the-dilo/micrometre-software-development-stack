7cba3b83fc59854bebf78907da389f8ca0870727
Darren <darren@micrometre.co.uk>
2016-03-25 17:01:18 +0000

    Update wordpress-docker.sh
    improve description

--- wordpress-docker.sh
+++ wordpress-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 docker run --name wordpressdb --restart=always -e MYSQL_ROOT_PASSWORD=0okmnbhu8 -e MYSQL_DATABASE=wordpress -d mysql
 docker run -e WORDPRESS_DB_PASSWORD=0okmnbhu8 -d --name wordpress --link wordpressdb:mysql  --restart=always -p 80:80 wordpress
-#plugins
+#plugins to improve site security
 #Antispam Bee
 #Rename wp-login.php
 #Wordfence Security
