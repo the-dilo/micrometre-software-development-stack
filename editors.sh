961f73078e6261d32cb6c00cc7f060ea4de6a268
Darren <darren@micrometre.co.uk>
2016-04-03 17:15:28 +0100

    Update editors.sh

--- editors.sh
+++ editors.sh
@@ -1,9 +1,4 @@
 /bin/sh
-#install sublime editor 
-add-apt-repository ppa:webupd8team/sublime-text-3 -y
-apt-get update
-apt-get install -y sublime-text
-apt-get update
 #atom editor
 add-apt-repository ppa:webupd8team/atom -y
 apt-get update
