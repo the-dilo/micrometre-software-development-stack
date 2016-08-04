5bda5399501cc1d7dd6c157a8f35cc32d60eb043
Henok <henokmicrometre@gmail.com>
2016-08-04 21:27:57 +0100

    Delete watir-firefox.rb

--- watir-firefox.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-require 'watir-webdriver'
-browser = Watir::Browser.new :firefox
-browser.goto "http://google.com"
-browser.text_field(:name => 'q').set("WebDriver rocks!")
-browser.button(:name => 'btnG').click
-puts browser.url
-browser.close
