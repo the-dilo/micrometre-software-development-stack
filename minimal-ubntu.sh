b14d797bfe7625ef13c253c68b7d2f02a1f5a977
Henok <henokmicrometre@gmail.com>
2017-04-25 21:11:32 +0100

    create minimal ubuntu 16.04

---
+++ minimal-ubntu.sh
@@ -0,0 +1,7 @@
+    1  apt-get install --no-install-recommends network-manager
+    2  clear
+    3  apt-get install --no-install-recommends ubuntu-desktop
+    4  apt-get install lightdm
+    5  apt-get install unity
+    6  apt-get install ubiquity-frontend-gtk
+    7  history
