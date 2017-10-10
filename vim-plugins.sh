43777845b96ea4ced5fb2d7f8bd1ca1d6e5fb5aa
Henok <henokmicrometre@gmail.com>
2017-10-10 12:25:53 +0100

    Update vim-plugins.sh

--- vim-plugins.sh
+++ vim-plugins.sh
@@ -2,25 +2,36 @@
 #Set up Vundle:
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #Configure Plugins Put this at the top of your .vimrc to use Vundle. 
+cat > .vimrc <<EOF
+set nocompatible              " be iMproved, required
+filetype off                  " required
 
+" set the runtime path to include Vundle and initialize
+ set rtp+=~/.vim/bundle/Vundle.vim
+ call vundle#begin()
+"let Vundle manage Vundle, required
+ Plugin 'VundleVim/Vundle.vim'
+"file manager"
+ Plugin 'scrooloose/nerdtree'
+"tern_for_vim"
+ Plugin 'marijnh/tern_for_vim'
+"git wraper"
+  Plugin 'tpope/vim-fugitive'
+"surround parent"
+  Plugin 'tpope/vim-surround'
+"completion"
+  Plugin 'valloric/youcompleteme'
+let g:ycm_server_keep_logfiles = 1
+let g:ycm_server_log_level = 'debug'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --tern-completer
+"open NERDTree with Ctrl+n"
+map <C-n> :NERDTreeToggle<CR>
+let g:airline#extensions#tabline#enabled = 1
+" Now we can turn our filetype functionality back on
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+filetype plugin indent on    " required  include
+EOF
+#cd ~/.vim/bundle/YouCompleteMe
+#./install.py --tern-completer
 
