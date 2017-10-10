4220d4195c03e942ed46f4dbdcbc15cb3b92409a
Henok <henokmicrometre@gmail.com>
2017-10-10 17:46:11 +0100

    fire fox without blot

--- post_install.sh
+++ post_install.sh
@@ -7,9 +7,15 @@ add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main
 add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner" 
 apt-get update
 apt-get upgrade
+#this section under contiual testing
 apt-get install xserver-xorg xserver-xorg-core xfonts-base xinit xterm --no-install-recommends
 apt-get install -y gnome-shell gnome-core gnome-system-tools gnome-app-install indicator-applet-appmenu gnome-session gnome-panel gnome-terminal --no-install-recommends
 apt-get install -y nautilus bleachbit gedit gedit-plugins vim tmux zsh git-core unzip openssh-server network-manager
+#firefox form mozila
+wget http://ftp.mozilla.org/pub/firefox/releases/53.0/linux-x86_64/en-US/firefox-53.0.tar.bz2
+tar xvf firefox*.tar.bz2
+mv firefox/ /opt
+ln -s /opt/firefox/firefox /usr/bin/firefox
 #Atom hackabl text editor.
 add-apt-repository ppa:webupd8team/atom -y
 apt-get update
