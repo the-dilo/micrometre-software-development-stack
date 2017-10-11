9e6fa789eef9374a8fd14729f64c4a5eea69e523
henok <henokmicrometre@gmail.com>
2017-10-11 01:09:19 +0000

    vim ide

--- vim-plugins.sh
+++ vim-plugins.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #Set up Vundle:
+cd ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #Configure Plugins Put this at the top of your .vimrc to use Vundle. 
-cat > .vimrc2 <<EOF
+cat > $HOME/.vimrc <<EOF
 set nocompatible              " be iMproved, required
 filetype off                  " required
 
@@ -32,8 +33,22 @@ let g:airline#extensions#tabline#enabled = 1
 call vundle#end()            " required
 filetype plugin indent on    " required  include
 EOF
-cp .vimrc ~/.vimrc3
 # change into plugin directorey and setup completion for javascript
+#git clone git@github.com:Valloric/YouCompleteMe.git
+vim -c 'PluginInstall' -c 'qa!'
+
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --tern-completer
+#This is a Vim plugin that provides Tern-based JavaScript editing support.
+cat > $HOME/.tern-project <<EOF
+.tern-project
+{
+    "libs" : ["ecma6", "browser"],
+    "plugins": {
+        "node" : {},
+        "es_modules" : {}
+    },
+    "ecmaVersion": 6
+}
+EOF
 
