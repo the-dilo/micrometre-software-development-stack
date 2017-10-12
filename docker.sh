c6c0035274e0c782ec6321ca27e95331e8d95b57
henok <henokmicrometre@gmail.com>
2017-10-12 00:41:16 +0000

    docker-ce installer for ubuntu 16.04

--- docker.sh
+++ docker.sh
@@ -9,11 +9,9 @@ apt-get update
 #Make sure you are about to install from the Docker repo instead of the default Ubuntu 16.04 repo:
 apt-cache policy docker-ce
 #install Docker:
-
 sudo apt-get install -y docker-ce
 # Check: the daemon started, and the process enabled to start on boot. 
 systemctl status docker
-
 #avoid typing sudo whenever you run the docker command, add your username to the docker group:
 usermod -aG docker ${USER}
 #To apply the new group membership, you can log out of the server and back in, or you can type the following:
@@ -21,8 +19,3 @@ su - ${USER}
 id -nG
 #check  Docker Hub
 docker run hello-world
-
-
-
-
-
