8ad1cba60660cf0212acae3d56fe71fea34a7cd7
henok <henokmicrometre@gmail.com>
2017-10-12 00:40:40 +0000

    docker-ce installer for ubuntu 16.04

---
+++ docker.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+apt-get update
+#Step 1 — Installing Docker add the GPG key for the official Docker repository to the system:
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+#Add the Docker repository to APT sources:
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#update the package database with the Docker packages from the newly added repo:
+apt-get update
+#Make sure you are about to install from the Docker repo instead of the default Ubuntu 16.04 repo:
+apt-cache policy docker-ce
+#install Docker:
+
+sudo apt-get install -y docker-ce
+# Check: the daemon started, and the process enabled to start on boot. 
+systemctl status docker
+
+#avoid typing sudo whenever you run the docker command, add your username to the docker group:
+usermod -aG docker ${USER}
+#To apply the new group membership, you can log out of the server and back in, or you can type the following:
+su - ${USER}
+id -nG
+#check  Docker Hub
+docker run hello-world
+
+
+
+
+
--- lxd.sh
+++ lxd.sh
@@ -12,5 +12,5 @@ Create the webserver container:
 #create a container that runs ubuntu 16.04 
 lxc launch ubuntu:x ubuntu_in_lxd
 #login to the newly created container
-lxc exec webserver -- sudo --login --user ubuntu
+lxc exec ubuntu_in_lxd -- sudo --login --user ubuntu
 
