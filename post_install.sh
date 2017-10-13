7932a8740753511589437f19edbc05b553ea5122
henok <henokmicrometre@gmail.com>
2017-10-13 10:47:11 +0000

    new comment

--- post_install.sh
+++ post_install.sh
@@ -10,9 +10,7 @@ add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) p
 apt-get update
 apt-get upgrade -y
 #add extra packages
-sudo add-apt-repository ppa:gnome3-team/gnome3-staging
-apt update
-apt dist-upgrade -y
+
 apt-get --no-install-recommends install -y gnome gnome-shell nautilus network-manager  
 #firefox form mozila
 wget http://ftp.mozilla.org/pub/firefox/releases/53.0/linux-x86_64/en-US/firefox-53.0.tar.bz2
