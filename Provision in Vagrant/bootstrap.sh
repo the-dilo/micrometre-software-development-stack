3d6f79f7582242003d8f469fc08b3a80cc783e0e
henok <henokmicrometre@gmail.com>
2017-10-14 01:37:20 +0000

    easy commit done

--- Provision in Vagrant/bootstrap.sh
+++ Provision in Vagrant/bootstrap.sh
@@ -6,13 +6,12 @@ apt-get install -y libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-open
 #Python, interactive object oriented language, for network programming, system administration,
 apt-get install -y python3.5 python-dev python3-dev python3-pip vim tmux zsh unzip openssh-server bash-completion 
 apt-get update
-#nodejs from source
-cd ~/ 
-git clone https://github.com/nodejs/node.git
-cd node
-git pull
-git checkout v6.9.2
-./configure && make && make install -j 3
+#install nodejs 
+curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+#The PPA will be added to your configuration After
+bash nodesource_setup.sh
+#The PPA will be added to your configuration After
+apt-get install nodejs
 export PATH=$PATH:/opt/node/bin
 #npm packages for webdrivers
 npm install -g express
@@ -42,5 +41,52 @@ rm -rf ruby-2.4.2
 rm -rf node
 apt-get install -y vim tmux zsh 
 apt-get update
+#Set up Vundle packege manager for vim  
+cd ~/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#Configure Plugins Put this at the top of your .vimrc to use Vundle. 
+cat > $HOME/.vimrc <<EOF
+set nocompatible              " be iMproved, required
+filetype off                  " required
+" set the runtime path to include Vundle and initialize
+ set rtp+=~/.vim/bundle/Vundle.vim
+ call vundle#begin()
+"let Vundle manage Vundle, required
+ Plugin 'VundleVim/Vundle.vim'
+"file manager"
+ Plugin 'scrooloose/nerdtree'
+"tern_for_vim"
+ Plugin 'marijnh/tern_for_vim'
+"git wraper"
+  Plugin 'tpope/vim-fugitive'
+"surround parent"
+  Plugin 'tpope/vim-surround'
+"completion"
+  Plugin 'valloric/youcompleteme'
+let g:ycm_server_keep_logfiles = 1
+let g:ycm_server_log_level = 'debug'
+"open NERDTree with Ctrl+n"
+map <C-n> :NERDTreeToggle<CR>
+let g:airline#extensions#tabline#enabled = 1
+" Now we can turn our filetype functionality back on
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+filetype plugin indent on    " required  include
+EOF
+git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && ./install.py --tern-completer
+#This is a Vim plugin that provides Tern-based JavaScript editing support.
+cat > $HOME/.tern-project <<EOF
+.tern-project
+{
+    "libs" : ["ecma6", "browser"],
+    "plugins": {
+        "node" : {},
+        "es_modules" : {}
+    },
+    "ecmaVersion": 6
+}
+EOF
+vim -c 'PluginInstall' -c 'qa!'
 
 
