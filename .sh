56cee4b1997948d6d32ca8a5d4f72446052a1e58
henok <henokmicrometre@gmail.com>
2017-10-14 18:34:24 +0000

    nvm rvm compiling

--- .sh
+++ .sh
@@ -9,3 +9,19 @@ It’ll take awhile to compile.
 rvm use 2.4.2 --default
 which ruby
 ruby -v
+
+
+
+
+#!/bin/bash
+bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
+source ~/.bash_profile
+rvm autolibs enable
+rvm install 1.9.3
+rvm use 1.9.3 --default
+rvm use 1.9.3@global
+echo "install: --no-rdoc --no-ri" >> ~/.gemrc
+echo "update: --no-rdoc --no-ri" >> ~/.gemrc
+gem install bundler
+gem install rake
