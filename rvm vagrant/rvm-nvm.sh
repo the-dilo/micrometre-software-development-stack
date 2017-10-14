750b5f7fb1d4e10dbc83ab901585806ddc0d95e9
henok <henokmicrometre@gmail.com>
2017-10-14 21:00:29 +0000

    rvn headeach

--- rvm vagrant/rvm-nvm.sh
+++ rvm vagrant/rvm-nvm.sh
@@ -9,7 +9,6 @@ nvm alias default 6.11.4
 bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
 source ~/.bash_profile
-rvm autolibs enable
 rvm install 2..4.2
 rvm use 2.4.2 --default
 rvm use 2.4.2@global
