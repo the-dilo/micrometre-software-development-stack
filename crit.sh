ad2206494de418f944c52ecee1b4da0b9ab71be9
henok <henokmicrometre@gmail.com>
2017-10-15 04:23:37 +0000

    node from hell

--- crit.sh
+++ crit.sh
@@ -3,7 +3,7 @@ sudo apt-get install build-essential libssl-dev
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
 bash install_nvm.sh 
 source ~/.profile 
-cat >> $HOME/.vimrc <<EOF
+cat >> $HOME/.bashrc <<EOF
 export NVM_DIR=~/.nvm
 source ~/.nvm/nvm.sh
 EOF
