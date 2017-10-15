e3a9378cbcf4d75ce40afcde38eb7275064c911d
henok <henokmicrometre@gmail.com>
2017-10-15 05:16:27 +0000

    no-comple_for nodejs-npm

--- crit.sh
+++
@@ -1,8 +0,0 @@
-sudo apt update 
-sudo apt-get install build-essential libssl-dev
-curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
-bash install_nvm.sh 
-echo 'export PATH=~/.nvm.sh:$PATH' >>~/.bash_profile
-source ~/.profile 
-EOF
-nvm -v
