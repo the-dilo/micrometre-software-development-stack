8fd6f368db6282da27e79d684bffd33fd92183ae
henok <henokmicrometre@gmail.com>
2017-10-15 05:20:16 +0000

    no-comple_for node

--- rvm vagrant/rvm-nvm.sh
+++ rvm vagrant/rvm-nvm.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash
-echo "sourc ~/.nvm/nvm.sh" >> /home/vagrant/.profile
-source /home/vagrant/.profile
-nvm install 6.11.4
-nvm alias default 6.11.4
-
-#!/bin/bash
 bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
 source ~/.bash_profile
