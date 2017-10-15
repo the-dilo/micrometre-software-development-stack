a428cd19dd6333e30b31d3572857500c2798d8a5
henok <henokmicrometre@gmail.com>
2017-10-15 04:33:21 +0000

    last one of-now

--- crit.sh
+++ crit.sh
@@ -2,9 +2,7 @@ sudo apt update
 sudo apt-get install build-essential libssl-dev
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
 bash install_nvm.sh 
+echo 'export PATH=~/.nvm.sh:$PATH' >>~/.bash_profile
 source ~/.profile 
-cat >> $HOME/.bashrc <<EOF
-export NVM_DIR=~/.nvm
-source ~/.nvm/nvm.sh
 EOF
 nvm -v
