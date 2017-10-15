898289880ef2ca7c59d44f78b651b8ccc2e4d6ac
henok <henokmicrometre@gmail.com>
2017-10-15 05:07:52 +0000

    no-comple_for nodejs takes-too-long

--- Provision _in _Vagrant/bootstrap.sh
+++ Provision _in _Vagrant/bootstrap.sh
@@ -17,59 +17,9 @@ npm install -g npm-check-updates
 
 #gems ruby packages
 gem install rails 5.1.4
-gem install tmuxinator
 gem install bundler
 gem install rake
 gem install rubygems-update
 #install shell packages to setup tthe IDE
 apt-get install -y vim tmux zsh 
 apt-get update
-#Set up Vundle packege manager for vim  
-cd ~/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#Configure Plugins Put this at the top of your .vimrc to use Vundle. 
-cat > $HOME/.vimrc <<EOF
-set nocompatible              " be iMproved, required
-filetype off                  " required
-" set the runtime path to include Vundle and initialize
- set rtp+=~/.vim/bundle/Vundle.vim
- call vundle#begin()
-"let Vundle manage Vundle, required
- Plugin 'VundleVim/Vundle.vim'
-"file manager"
- Plugin 'scrooloose/nerdtree'
-"tern_for_vim"
- Plugin 'marijnh/tern_for_vim'
-"git wraper"
-  Plugin 'tpope/vim-fugitive'
-"surround parent"
-  Plugin 'tpope/vim-surround'
-"completion"
-  Plugin 'valloric/youcompleteme'
-let g:ycm_server_keep_logfiles = 1
-let g:ycm_server_log_level = 'debug'
-"open NERDTree with Ctrl+n"
-map <C-n> :NERDTreeToggle<CR>
-let g:airline#extensions#tabline#enabled = 1
-" Now we can turn our filetype functionality back on
-" All of your Plugins must be added before the following line
-call vundle#end()            " required
-filetype plugin indent on    " required  include
-EOF
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && ./install.py --tern-completer
-#This is a Vim plugin that provides Tern-based JavaScript editing support.
-cat > $HOME/.tern-project <<EOF
-.tern-project
-{
-    "libs" : ["ecma6", "browser"],
-    "plugins": {
-        "node" : {},
-        "es_modules" : {}
-    },
-    "ecmaVersion": 6
-}
-EOF
-vim -c 'PluginInstall' -c 'qa!'
-
-
--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) univ
 add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner" 
 apt-get update
 apt-get upgrade -y
+#uncoment this section fo desktop applications as needed 
 #testing new GUI replacing unity
 #apt-get --no-install-recommends install -y gnome gnome-shell nautilus network-manager  
 #firefox form mozila
@@ -40,6 +41,7 @@ cd ~
 curl -sL https://deb.nodesource.com/setup_6.11.4 -o nodesource_setup.sh
 #And run the script
 bash nodesource_setup.sh
+apt-get update
 apt-get install nodejs
 node -v
 #install packages  install it globally add the -g flag:
@@ -48,3 +50,19 @@ npm-install -g learnyounode
 npm install -g jason
 npm install -g npm-check-updates
 #ruby from source LTS using RVM
+cd
+wget http://ftp.ruby-lang.org/pub/ruby/2.4/ruby-2.4.2.tar.gz
+tar -xzvf ruby-2.4.2.tar.gz
+cd ruby-2.4.2/
+./configure
+make
+sudo make install
+ruby -v
+#gems ruby packages
+gem install rails 5.1.4
+gem install bundler
+gem install rake
+gem install rubygems-update
+#install shell packages to setup tthe IDE
+apt-get install -y vim tmux zsh 
+apt-get update
