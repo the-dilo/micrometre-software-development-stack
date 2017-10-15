8de8ce2d616f0b1b0755abf748017233dfbc4495
henok <henokmicrometre@gmail.com>
2017-10-15 04:12:36 +0000

    one-step mybe forward

---
+++ crit.sh
@@ -0,0 +1,10 @@
+sudo apt update 
+sudo apt-get install build-essential libssl-dev
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
+bash install_nvm.sh 
+source ~/.profile 
+cat > $HOME/.vimrc <<EOF
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
+EOF
+nvm -v
