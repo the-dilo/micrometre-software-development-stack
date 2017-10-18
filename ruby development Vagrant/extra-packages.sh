f0121ce9cb0e69e6bd31fa271fe7800bddd3baa5
henok <henokmicrometre@gmail.com>
2017-10-18 01:22:54 +0000

    added vim rc

--- ruby development Vagrant/extra-packages.sh
+++ ruby development Vagrant/extra-packages.sh
@@ -9,13 +9,33 @@ node -v
 #install shell packages 
 apt-get install -y vim tmux zsh 
 apt-get update
-#setp vim with plugins manager with plugins
+cd ~/
+chsh -s /usr/bin/zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+#!/bin/bash
+#Set up Vundle:
 cd ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp .vimrc /home/vagrant/.vimrc
-chown -R vagrant:vagrant /home/vagrant/.vimrc
+#Configure Plugins Put this at the top of your .vimrc to use Vundle. 
+cat > $HOME/.vimrc <<EOF
+set nocompatible              " be iMproved, required
+filetype off                  " required
+" set the runtime path to include Vundle and initialize
+ set rtp+=~/.vim/bundle/Vundle.vim
+ call vundle#begin()
+"let Vundle manage Vundle, required
+ Plugin 'VundleVim/Vundle.vim'
+"file manager"
+ Plugin 'scrooloose/nerdtree'
+"tern_for_vim"
+"open NERDTree with Ctrl+n"
+map <C-n> :NERDTreeToggle<CR>
+let g:airline#extensions#tabline#enabled = 1
+" Now we can turn our filetype functionality back on
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+filetype plugin indent on    " required  include
+EOF
 vim -c 'PluginInstall' -c 'qa!'
-chsh -s $(which zsh) 
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
 
