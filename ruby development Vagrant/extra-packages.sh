9d9b66ccdb4754cfb3bf5eacce0b4f80274d4083
henok <henokmicrometre@gmail.com>
2017-10-18 03:09:07 +0000

    addeing ide sdk

--- ruby development Vagrant/extra-packages.sh
+++ ruby development Vagrant/extra-packages.sh
@@ -12,29 +12,4 @@ apt-get update
 cd ~/
 chsh -s /usr/bin/zsh
 cd ~/
-git clone https://github.com/VundleVim/Vundle.vim.git /home/vagrant/vim/bundle/Vundle.vim
-#Configure Plugins Put this at the top of your .vimrc to use Vundle. 
-cat >  /home/vagrant/.vimrc <<EOF
-set nocompatible              " be iMproved, required
-filetype off                  " required
-" set the runtime path to include Vundle and initialize
- set rtp+=~/.vim/bundle/Vundle.vim
- call vundle#begin()
-"let Vundle manage Vundle, required
- Plugin 'VundleVim/Vundle.vim'
-"file manager"
- Plugin 'scrooloose/nerdtree'
-"tern_for_vim"
-"open NERDTree with Ctrl+n"
-map <C-n> :NERDTreeToggle<CR>
-let g:airline#extensions#tabline#enabled = 1
-" Now we can turn our filetype functionality back on
-" All of your Plugins must be added before the following line
-call vundle#end()            " required
-filetype plugin indent on    " required  include
-EOF
-vim -c 'PluginInstall' -c 'qa!'
-#oh-my-zsh.git insatal
-git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
-cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
-chsh -s /usr/bin/zsh vagrant
+
